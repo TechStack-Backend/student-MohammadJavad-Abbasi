b744452b92236bbca2a266befc150ac2cde242e4
Crab01 <mjad337@gmail.com>
2025-08-14 17:46:14 +0330

    fix(task-1): deleted wrong added folder

--- hello_world/say_my_name/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SayMyNameConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'say_my_name'
--- hello_world/say_my_name/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-def say_my_name(request):
-    return HttpResponse("Hello Javad")
